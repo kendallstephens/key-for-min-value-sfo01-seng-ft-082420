095df301f66c4e170d372381732d40a678ce79d9
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-07 16:16:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,7 @@
 def key_for_min_value(name_hash)
   name_min = name_hash.to_a
   max_value = 1000
-  lowest_value_key = 0 
-  lowest_value = nil
+  lowest_value_key = nil
   name_min.each do |i|
     if i[1] < max_value
       max_value = i[1]
